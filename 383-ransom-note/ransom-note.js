0fd4b42361e3d2af3ca3677dca559ec2734881bc
Salmanul Faris <135725855+salmanulfariskk@users.noreply.github.com>
2024-04-18 11:12:17 +0530

    Time: 72 ms (61.02%) | Memory: 53.4 MB (30.39%) - LeetSync

---
+++ 383-ransom-note/ransom-note.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+ const charCount = {};
+    for (const char of magazine) {
+        charCount[char] = (charCount[char] || 0) + 1;
+        
+    }
+    for (const char of ransomNote) {
+        if (!charCount[char]) {
+            
+            return false;
+        }
+        charCount[char]--;
+
+    }
+
+    return true;
+};
