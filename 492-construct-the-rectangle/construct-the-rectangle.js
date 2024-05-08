848be2611aeb7e48d52fb369c934a41a62f30b7f
Salmanul Faris <135725855+salmanulfariskk@users.noreply.github.com>
2024-05-08 10:36:03 +0530

    Time: 51 ms (77.16%) | Memory: 48.7 MB (80.25%) - LeetSync

---
+++ 492-construct-the-rectangle/construct-the-rectangle.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function(area) {
+     let width = Math.floor(Math.sqrt(area));
+    while (area % width !== 0) {
+        width--;
+    }
+    return [area / width, width];
+};
