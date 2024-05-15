339c91826da1ee270ec26a38fd2eaa2216db84e8
Salmanul Faris <135725855+salmanulfariskk@users.noreply.github.com>
2024-05-15 17:59:14 +0530

    Time: 42 ms (93.68%) | Memory: 48.3 MB (80.53%) - LeetSync

---
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function(a, b) {
+    const lengthA = a.length
+    const lengthB = b.length
+
+    if(a===b){
+        return -1
+    }
+
+    if(lengthA>lengthB){
+        return lengthA
+    }else{
+        return lengthB
+    }
+
+    
+
+};
