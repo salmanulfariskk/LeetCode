786e65c2e5b869c035a8a9fe49540d7c5144f77c
Salmanul Faris <135725855+salmanulfariskk@users.noreply.github.com>
2024-07-23 08:59:02 +0530

    Time: 88 ms (25.19%) | Memory: 53.4 MB (86.58%) - LeetSync

--- 2502-sort-the-people/sort-the-people.js
+++ 2502-sort-the-people/sort-the-people.js
@@ -8,12 +8,11 @@ var sortPeople = function (names, heights) {
     for (let i = 0; i < heights.length; i++) {
         for (let j = 0; j < heights.length - 1; j++) {
             if (heights[j] < heights[j + 1]) {
-                // Swap heights
+                
                 let tempHeight = heights[j];
                 heights[j] = heights[j + 1];
                 heights[j + 1] = tempHeight;
 
-                // Swap corresponding names
                 let tempName = names[j];
                 names[j] = names[j + 1];
                 names[j + 1] = tempName;
