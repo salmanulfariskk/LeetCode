ee48ea0ce1c7f6f959a4021f921052adac5ed563
Salmanul Faris <135725855+salmanulfariskk@users.noreply.github.com>
2024-08-01 09:19:30 +0530

    Time: 50 ms (90.02%) | Memory: 50.4 MB (64.57%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -2,16 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
-    let arr = []
-    for(let i=0;i<nums.length;i++){
-        if(i===0){
-            arr.push(nums[i])
-        }else{
-
-        let sum = nums[i]+arr[i-1]
-        arr.push(sum)
-        }
-    }
-    return arr
+var runningSum = function (nums) {
+    let cumulativeSum = 0;
+    return nums.map(num => cumulativeSum += num)
 };
