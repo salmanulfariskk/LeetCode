f89fb22b020704c68cb5b8ec7825b66fae027c7b
Salmanul Faris <135725855+salmanulfariskk@users.noreply.github.com>
2024-09-03 10:50:18 +0530

    Time: 74 ms (5.29%) | Memory: 53.4 MB (5.32%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let cur = strs[0]
+    let temp = ""
+    for (let i = 1; i < strs.length; i++) {
+        for (let j = 0; j < cur.length; j++) {
+            if (cur[j] == strs[i][j]) {
+                temp += cur[j]
+            } else {
+                break
+            }
+        }
+        cur = temp
+        temp = ""
+    }
+    return cur
+};
